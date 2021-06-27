087291524b68308bf5c6c3b9c3209d26b0ee19ab
meretamal <meretamal@uc.cl>
2021-06-27 15:11:39 -0400

    feat(author-detail): add jsonplaceholder request to author detail view

--- src/views/AuthorDetail.jsx
+++ src/views/AuthorDetail.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Books from '../components/Books';
 
 export default function AuthorDetail() {
   const { id } = useParams();
+  const [author, setAuthor] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          setError(true);
+          return {};
+        }
+        return response.json();
+      })
+      .then(setAuthor)
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <div>
       <Link to="/authors">Authors</Link>
-      <h2>{`Author ${id}`}</h2>
-      <Books />
+      {error ? (
+        <h2>Something went wrong, please try again later</h2>
+      ) : (
+        <div>
+          <h2>{author.name}</h2>
+          <Books />
+        </div>
+      )}
     </div>
   );
 }
