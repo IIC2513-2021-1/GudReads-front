c1fb47a1572a340447350bca7758afc2816788a0
meretamal <meretamal@uc.cl>
2021-06-27 15:11:17 -0400

    feat(author-list): add jsonplaceholder request to author list view

--- src/views/AuthorList.jsx
+++ src/views/AuthorList.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function AuthorList() {
-  const authors = [
-    { id: 1 },
-    { id: 2 },
-  ];
+  const [authors, setAuthors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        if (response.status !== 200) {
+          setError(true);
+          return [];
+        }
+        return response.json();
+      })
+      .then(setAuthors)
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <div>
       <Link to="/">Home</Link>
-      <h2>Authors</h2>
-      {authors.map(({ id }) => <div key={id}><Link to={`/authors/${id}`}>{`Author ${id}`}</Link></div>)}
+      {error ? (
+        <h2>Something went wrong, please try again later</h2>
+      ) : (
+        <div>
+          <h2>Authors</h2>
+          {authors.map(({ id, name }) => <div key={id}><Link to={`/authors/${id}`}>{name}</Link></div>)}
+        </div>
+      )}
     </div>
   );
 }
